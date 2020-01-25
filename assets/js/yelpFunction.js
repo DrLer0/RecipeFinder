dc47a8f640d37337c3c35a891a975e026f426145
Jonathan Doctolero <jonathan.doctolero7@gmail.com>
2020-01-24 18:22:08 -0800

    yelpFunction returns restuarants object

--- assets/js/yelpFunction.js
+++ assets/js/yelpFunction.js
@@ -25,26 +25,39 @@ function searchRestaurants(foodItem, location) {
                 // Itirate through the JSON array of 'businesses' which was returned by the API
                 $.each(data.businesses, function(i, item) {
                     // Store each business's object in a variable
-                    var id = item.id;
-                    // var alias = item.alias;
-                    var phone = item.display_phone;
-                    var image = item.image_url;
-                    var name = item.name;
-                    var rating = item.rating;
-                    var reviewcount = item.review_count;
-                    var address = item.location.address1;
-                    var city = item.location.city;
-                    var state = item.location.state;
-                    var zipcode = item.location.zip_code;
-                    // Append our result into our page
-                    $('#results').append('<div id="' + id + '" style="margin-top:50px;margin-bottom:50px;"><img src="' + image + '" style="width:200px;height:150px;"><br>We found <b>' + name + '</b><br>Business ID: ' + id + '<br> Located at: ' + address + ' ' + city + ', ' + state + ' ' + zipcode + '<br>The phone number for this business is: ' + phone + '<br>This business has a rating of ' + rating + ' with ' + reviewcount + ' reviews.</div>');
+                    var restaurants = {
+                        id: item.id,
+                        phone: item.display_phone,
+                        image: item.image_url,
+                        name: item.name,
+                        rating: item.rating,
+                        reviewcount: item.review_count,
+                        address: item.location.address1,
+                        city: item.location.city,
+                        state: item.location.state,
+                        zipcode: item.location.zip_code
+                    }
+                    return restaurants;
                 });
             } else {
                 // If our results are 0; no businesses were returned by the JSON therefor we display on the page no results were found
                 $('#results').append('<h5>We discovered no results!</h5>');
+                var restaurants = {
+                    id: null,
+                    phone: null,
+                    image: null,
+                    name: null,
+                    rating: null,
+                    reviewcount: null,
+                    address: null,
+                    city: null,
+                    state: null,
+                    zipcode: null
+                }
+                return restaurants;
             }
         }
     });
 }
 
-searchRestaurants("banana", "boston");
+// console.log(searchRestaurants("banana", "boston"));
